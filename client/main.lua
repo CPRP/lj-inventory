87e0f03055a97750072c288ebef6996954fffb6d
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-09-15 19:51:26 -0400

    edited line 555 for weapon_petrolcan

--- client/main.lua
+++ client/main.lua
@@ -552,11 +552,9 @@ RegisterNetEvent('inventory:client:UseWeapon', function(weaponData, shootbool)
     else
         TriggerEvent('weapons:client:SetCurrentWeapon', weaponData, shootbool)
         local ammo = tonumber(weaponData.info.ammo) or 0
-
-        if weaponName == "weapon_fireextinguisher" then
+        if weaponName == "weapon_petrolcan" or "weapon_fireextinguisher" then
             ammo = 4000
         end
-
         GiveWeaponToPed(ped, weaponHash, ammo, false, false)
         SetPedAmmo(ped, weaponHash, ammo)
         SetCurrentPedWeapon(ped, weaponHash, true)
